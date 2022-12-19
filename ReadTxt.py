9f83a590357c75490719228b9b1903d1483d7190
Alex <alexiv.04@mail.ru>
2022-12-19 09:39:56 +0700

    refr main vivod

--- ReadTxt.py
+++ ReadTxt.py
@@ -32,6 +32,7 @@ def rowOutput(row):
         my_file = open("DataPeople.txt", encoding='utf8')
         s: list = my_file.readlines()
         a: int = 0
+    
 
         while a < row:
             line: str = len(s[a])
@@ -49,18 +50,52 @@ def rowOutput(row):
     except:
         print("Шото пошло не так")
 
+def gat_table_result(row):
+    try:
+        my_table = PrettyTable(["id", "last_name", "first_name", "middle_name", "age"])
+        my_file = open("DataPeople.txt", encoding='utf8')
+        s: list = my_file.readlines()
+        a: int = 0
+
+        if (row == 'all'):
+            sravn = len(s)
+        else:
+            sravn = row
+
+        while a < sravn:
+            line: str = len(s[a])
+            if a + 1 == len(s):
+                id_user, surname, name, patronymic, age = map(str, s[a].split())
+                my_table.add_row([id_user, surname, name, patronymic, age])
+            else:
+                remove_last: str = s[a][:line - 1]
+                id_user, surname, name, patronymic, age = map(str, remove_last.split())
+                my_table.add_row([id_user, surname, name, patronymic, age])
+            a += 1
+        print(my_table)
+    except SyntaxError:
+        print("Ошибка чтения файла!")
+    except:
+        print("Что-то пошло не так ...")
+
 
 if __name__ == '__main__':
     while True:
         try:
             strok = input("show data all для вывода всех записей, show data n, где n количество записей\n")
             try:
-                result = re.search(r'\d', strok)
-                rowOutput(int(result.group(0)))
+                result = re.search(r'show data (\d+)', strok)
+                if result:
+                    records = strok.split(' ')
+                    gat_table_result(int(records[2]))
+                else:
+                    result = re.search(r'show data all', strok)
+                    if result.group(0) == "show data all":
+                        records = result.group(0).split(' ')
+                        gat_table_result(records[2])
             except:
-                result = re.search(r'show data all', strok)
-                if result.group(0) == "show data all":
-                    rowAll()
+                print("Что-то пошло не так ...")
+                
         except AttributeError:
             print("такой команды нет")
         except KeyboardInterrupt:
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-# Получение данных от пользователя
+from prettytable import PrettyTable
+import re
+
+
+""" Получение данных от пользователя """
 def getData():
     while True:
         try:
-
-            print("Введите 'y' для добавления данных, и 'n' для завершения")
-            check: str = input()
+            check: str = input("Введите 'y' для добавления данных, 'n' для завершения, show data all для вывода всех записей и show data n, где n количество записей: ")
+            #check: str = input()
+            result_n = re.search(r'show data (\d+)', check)
+            result_all = re.search(r'show data all', check)
             if check in ["y", "Y", "yes", "Yes", "YES"]:
                 try:
                     print('Введите номер, ФИО и возраст:')
@@ -27,10 +32,52 @@ def getData():
                     print('Ошибка!')
                 finally:
                     my_file.close()  
+            elif result_n:
+                records = check.split(' ')
+                gat_table_result(int(records[2]))
+            elif result_all:
+                if result_all.group(0) == "show data all":
+                    records = result_all.group(0).split(' ')
+                    gat_table_result(records[2])
+            elif check in ["n", "N", "no", "No", "NO"]:
+                print("Выход")
+                break
             else:
-                exit()
+                print('Нет такой команды')
+
         except KeyboardInterrupt:
             print('Выход')
+            exit()
+        except:
+            print("Что-то пошло не так ...")
+
+def gat_table_result(row):
+    try:
+        my_table = PrettyTable(["id", "last_name", "first_name", "middle_name", "age"])
+        my_file = open("DataPeople.txt", encoding='utf8')
+        s: list = my_file.readlines()
+        a: int = 0
+
+        if (row == 'all'):
+            sravn = len(s)
+        else:
+            sravn = row
+
+        while a < sravn:
+            line: str = len(s[a])
+            if a + 1 == len(s):
+                id_user, surname, name, patronymic, age = map(str, s[a].split())
+                my_table.add_row([id_user, surname, name, patronymic, age])
+            else:
+                remove_last: str = s[a][:line - 1]
+                id_user, surname, name, patronymic, age = map(str, remove_last.split())
+                my_table.add_row([id_user, surname, name, patronymic, age])
+            a += 1
+        print(my_table)
+    except SyntaxError:
+        print("Ошибка чтения файла!")
+    except:
+        print("Что-то пошло не так ...")
 
 if __name__ == '__main__':
     getData()
